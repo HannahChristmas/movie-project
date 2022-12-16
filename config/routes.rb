2f2de4bf693b511be1cd99490de78b90e0bc47b9
HannahChristmas <hc109909@gmail.com>
2022-12-16 17:59:20 -0500

    User controller create method

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :movies
   resources :users
   # Routing logic: fallback requests for React Router.
+  post 'signup', to: 'users#create'
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
