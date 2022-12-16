dd78c326b3697d8b279b5271fdeebf9556d569a9
HannahChristmas <hc109909@gmail.com>
2022-12-16 18:00:04 -0500

    Application controller RecordInvalid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordInvalid, with: :handle_invalid
+
+  private 
+  
+  def handle_invalid(invalid) 
+    render json: { errors: invalid.record.errors.full_messages}, status: 422
+  end
 end
