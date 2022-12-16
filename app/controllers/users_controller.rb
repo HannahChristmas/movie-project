d3b6b21cfd49196f32093f919c23783b0c7ad6e3
HannahChristmas <hc109909@gmail.com>
2022-12-16 18:03:55 -0500

    user controller def create update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
     skip_before_action :authorize, only: :create 
-    
+
     def create 
         user = User.create!(user_params)
         session[:user_id] = user.id 
         render json: user, status: 201
+    end
+
+    private 
+
+    def user_params 
+        params.permit(:username, :password, :password_confirmation, :image_url)
     end
 
 end
