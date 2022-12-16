86fffadf800c2ad4dc7391f31bf69591c11a117d
HannahChristmas <hc109909@gmail.com>
2022-12-16 17:33:15 -0500

    edit user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
 
-    has_many :movies
-    has_many :reviews, through: :movies
+    has_many :reviews
+    has_many :movies, through: :reviews
 
     validates :name, presence: true, uniqueness: true
 end
