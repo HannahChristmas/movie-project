43682acad72beb0bb1ea36302a1ab783c67c74f8
HannahChristmas <hc109909@gmail.com>
2022-12-16 18:02:40 -0500

    set up login

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   resources :movies
   resources :users
   # Routing logic: fallback requests for React Router.
-  post 'signup', to: 'users#create'
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
