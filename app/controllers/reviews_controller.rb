817aa9b7a5d40be0fea7e887cbfe71884e20990a
HannahChristmas <hc109909@gmail.com>
2022-12-20 10:23:43 -0500

    Update controllers

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,27 @@
 class ReviewsController < ApplicationController
 rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
+    def index
+        reviews = @current_user.reviews.all 
+        render json: reviews 
+    end
+
     def create 
-        review = Review.create!(review_params)
+        review = @current_user.review.create!(review_params)
         render json: review.movie, status: 201
     end
 
+    def update
+        review = @current_user.reviews.find(params[:id])
+        review.update!(review_params)
+        render json: review
+    end
     
+    def destroy 
+        review = @current_user.reviews.find(params[:id])
+        review.destroy 
+        head :no_content
+    end
 
     private 
 
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,6 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :review, :user_id, :movie_id
   
-  # belongs_to :user 
+  belongs_to :user 
+  belongs_to :movie
 end
